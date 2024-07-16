d9db2b659217030d14504c8bdf8c8cfb8cee998a
Fernando De Leon Gil <gasnare@hotmail.com>
2024-07-16 16:10:59 -0500

    Add interaction for get data and render in component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
 import "../styles/List.css";
 
-export const List = () => {
+export const List = ({ todayDate }) => {
+  const KEY = "9KXvxSH3iyplMyjdgriSgwlUIdh7WuWPOVH05lwT";
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-
-  const KEY = "9KXvxSH3iyplMyjdgriSgwlUIdh7WuWPOVH05lwT";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `https://api.nasa.gov/planetary/apod?api_key=${KEY}`
+          `https://api.nasa.gov/planetary/apod?api_key=${KEY}${
+            todayDate !== null ? `&date=${todayDate}` : ""
+          }`
         );
         if (!response.ok) {
           throw new Error("Network response was not ok.");
@@ -28,13 +30,12 @@ export const List = () => {
     };
 
     fetchData();
-  }, []);
+  }, [todayDate]);
 
-  if (loading) return <p>Cargando ...</p>;
+  if (loading) return <p className="nasa-otherText">Cargando ...</p>;
 
-  if (error) return <p>Hubo un error: {error.message}</p>;
-
-  console.log(data);
+  if (error)
+    return <p className="nasa-otherText">Hubo un error: {error.message}</p>;
 
   return (
     <main className="nasa-main-container">
@@ -42,20 +43,22 @@ export const List = () => {
         <h1 className="nasa-title">{data.title}</h1>
         <p className="nasa-text">{data.explanation}</p>
       </div>
+      <div className="nasa-filter">
+        <input type="date" />
+      </div>
       <div className="nasa-container-image">
-        <div
-          className="nasa-image"
-          style={{ backgroundImage: `url(${data.url})` }}
-        />
-        <div
-          className="nasa-image"
-          style={{ backgroundImage: `url(${data.url})` }}
-        />
-        <div
-          className="nasa-image"
-          style={{ backgroundImage: `url(${data.url})` }}
-        />
+        {data.map((info) => (
+          <div
+            key={info.url}
+            className="nasa-image"
+            style={{ backgroundImage: `url(${info.url})` }}
+          />
+        ))}
       </div>
     </main>
   );
+};
+
+List.propTypes = {
+  todayDate: PropTypes.string.isRequired,
 };
