2b4e747477b3333f4e1e9d67a26e662339bf6d77
Fernando De Leon Gil <gasnare@hotmail.com>
2024-07-16 16:00:23 -0500

    Add button filter date now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
+import PropTypes from "prop-types";
+
 import "../styles/Header.css";
 
-export const Header = () => {
+export const Header = ({ dateToday, handleToday }) => {
   return (
     <header className="nasa-headerContainer">
-      <button className="nasa-button .about">ABOUT US</button>
+      <button
+        type="button"
+        className="nasa-button .today"
+        onClick={handleToday}
+      >
+        {dateToday !== null ? `Foto del día: ${dateToday}` : "Imagen del día"}
+      </button>
       <button className="nasa-button .search">SEARCH</button>
       <button className="nasa-button .contact">CONTACT</button>
     </header>
   );
+};
+
+Header.propTypes = {
+  dateToday: PropTypes.string.isRequired,
+  handleToday: PropTypes.func.isRequired,
 };
