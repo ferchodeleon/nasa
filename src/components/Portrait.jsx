8b2ecd1b9d43a1df1f01baa4a90a5b1fd255f3d2
Fernando De Leon Gil <gasnare@hotmail.com>
2024-09-12 15:25:24 -0500

    Add new component to portrait of page

---
+++ src/components/Portrait.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef } from "react";
+import "../styles/Portrait.css";
+
+export const Portrait = () => {
+  const typewriteRef = useRef(null);
+
+  const restartAnimation = () => {
+    const element = typewriteRef.current;
+    element.style.animation = "none";
+    setTimeout(() => (element.style.animation = ""), 1000);
+  };
+
+  useEffect(() => {
+    const element = typewriteRef.current;
+
+    const handleAnimationEnd = () => {
+      setTimeout(restartAnimation, 1000);
+    };
+
+    element.addEventListener("animationend", handleAnimationEnd);
+
+    return () => {
+      element.removeEventListener("animationend", handleAnimationEnd);
+    };
+  }, []);
+
+  return (
+    <>
+      <section className="portrait-container">
+        <h1>BEYOND THE STARS</h1>
+        <p ref={typewriteRef}>Capturing Space and NASA´s wonders</p>
+      </section>
+    </>
+  );
+};
