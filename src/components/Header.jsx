61c4be5e5010678846d65814f2e174ae4a9d4c4e
Fernando De Leon Gil <gasnare@hotmail.com>
2024-09-12 15:21:28 -0500

    Change image header, incluid new title and description animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,46 @@ import PropTypes from "prop-types";
 
 import { FilterComponent } from "./FilterComponent";
 
-import ImageMoon from "../assets/icon/icon-planet.png";
+import IconSpanish from "../assets/img/spanish.webp";
+import IconEnglish from "../assets/img/english.png";
 import "../styles/Header.css";
+import { useTranslation } from "react-i18next";
 
 export const Header = ({ dateToday, handleToday }) => {
+  const [t, i18n] = useTranslation("global");
+
+  const handleChangeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  };
+
+  const isSelected = () => {
+    const language = i18n.language;
+    console.log("language", language);
+    return language;
+  };
+
+  console.log(isSelected());
+
   return (
     <header className="nasa-headerContainer">
-      <img src={ImageMoon} alt="Icon planet" className="header-image" />
+      <div className="nasa-top-header">
+        <div className="nasa-translate">
+          <button
+            className={isSelected() === "es" ? "selected" : ""}
+            onClick={() => handleChangeLanguage("es")}
+          >
+            <img src={IconSpanish} alt="icon spanish" />
+            <p>{t("header.spanish")}</p>
+          </button>
+          <button
+            className={isSelected() === "en" ? "selected" : ""}
+            onClick={() => handleChangeLanguage("en")}
+          >
+            <img src={IconEnglish} alt="icon english" />
+            <p>{t("header.english")}</p>
+          </button>
+        </div>
+      </div>
       <FilterComponent dateToday={dateToday} handleToday={handleToday} />
     </header>
   );
